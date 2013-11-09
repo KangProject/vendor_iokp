c4dd290b9236a47a658429377669e295f7cd05e6
mrimp <mrimp@day-off.net>
2013-11-09 15:18:52 -0500

    Update common.mk

--- configs/common.mk
+++ configs/common.mk
@@ -22,7 +22,7 @@ PRODUCT_PACKAGES += \
     Focal \
     DashClock \
     CMFileManager \
-    Halo
+    HALO
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
