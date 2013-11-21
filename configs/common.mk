d830bf904b4a56d2d9b31025d1d4b95cb1025749
mrimp <mrimp@day-off.net>
2013-11-20 20:35:59 -0500

    jfltevzw: update fingerprint VRUEMJ7 Bumped version to 2.0

--- configs/common.mk
+++ configs/common.mk
@@ -105,8 +105,8 @@ PRODUCT_COPY_FILES += \
     vendor/iokp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # IOKP RELEASE VERSION
-IOKP_VERSION_MAJOR = v1
-IOKP_VERSION_MINOR = 5
+IOKP_VERSION_MAJOR = v2
+IOKP_VERSION_MINOR = 0
 # IOKP_VERSION_MAINTENANCE = RC1
 
 
