abcb4ed90556d8eb2de27290dd0ffc145d444241
mrimp <mrimp@day-off.net>
2014-01-06 05:51:07 -0500

    Bumped version 2

--- configs/common.mk
+++ configs/common.mk
@@ -124,7 +124,7 @@ PRODUCT_COPY_FILES += \
     vendor/iokp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # IOKP RELEASE VERSION
-IOKP_VERSION_MAJOR = v1
+IOKP_VERSION_MAJOR = v2
 # IOKP_VERSION_MINOR = 0
 
 
